ebfc819a2eb04970b5e54837165df889acdb226d
ustalol <david.vut@mail.ru>
2025-03-12 20:06:27 +0300

    upgraded hello_world.cpp to ask for name

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,5 +1,10 @@
 include <iostream>
+include <string>
 using namespace std;
 int main() {
 cout << "Hello world!";
+string name;
+cout << "Name";
+cin >> name;
+cout << "Hello world from " << name;
 }
