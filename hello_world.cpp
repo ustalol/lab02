0f5f34aa4c7f77ac3616a326dcbd9e42cac5cea9
ustalol <david.vut@mail.ru>
2025-03-19 00:33:52 +0300

    Update hello_world.cpp

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,20 +1,10 @@
 #include <iostream>
 #include <string>
-<<<<<<< HEAD
-int main() {
-//Комментарий, для части 3 пункт 4!
-std::string name;
-std::cout << "Name";
-std::cin >> name;
-std::cout << "Hello world from " << name;
-=======
-int
-main()
+int main()
 {
   // Комментарий
   std::string name;
   std::cout << "Name";
   std::cin >> name;
   std::cout << "Hello world from " << name;
->>>>>>> ce8672e (changed codestyle)
 }
